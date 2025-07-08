acdfdde0a008fbc698765853fe60fd3b43b355cc
Yosef Steinberg <steinbergyosef@gmail.com>
2025-07-08 15:05:56 +0300

    added middleware

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const PORT = 5050
 const server = express();
 
 
+function logger(req,res,next){
+    console.log(`got req to: ${req.method}, url: ${req.url}`);
+}
+
+
+server.use(logger)
+
+
 server.get('/greet',(req,res) => {
     res.send(JSON.stringify({message:`hi from get endpoint, ${new Date().toLocaleTimeString()}`}))
 })
